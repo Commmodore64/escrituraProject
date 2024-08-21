76c631f22ab46198f7556825e4ce5dabfc092523
commodore_64 <cesarml2002@hotmail.com>
2024-08-21 17:51:37 -0600

    v1.1 bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,36 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-const basenameProd = '/react-shadcn-starter'
+const basenameProd = '/react-shadcn-starter';
 
 export default defineConfig(({ command }) => {
-  const isProd = command === 'build'
+  const isProd = command === 'build';
 
   return {
     base: isProd ? basenameProd : '',
     plugins: [react()],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     define: {
-      global: {
-        basename: isProd ? basenameProd : '',
+      global: 'globalThis',
+    },
+    build: {
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+            return;
+          }
+          warn(warning);
+        },
+        commonjsOptions: {
+          include: /node_modules/,
+        },
+        external: ['axios'],
       },
     },
-  }
-})
+  };
+});
